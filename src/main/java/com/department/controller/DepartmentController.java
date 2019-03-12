249fef0c1485d142ee5ff723da581ce01155f536
Elijah Ayeeta <elijate@MACBOOKs-MacBook-Pro.local>
2019-03-12 15:47:15 +0300

    Add crud functionality

--- src/main/java/com/department/controller/DepartmentController.java
+++ src/main/java/com/department/controller/DepartmentController.java
@@ -1,5 +1,52 @@
 package com.department.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javassist.tools.web.BadHttpRequest;
+
+import com.department.entity.*;
+import com.department.repository.*;
+
+
 public class DepartmentController {
 
+	@Autowired
+    private DepartmentRepository repository;
+
+    @GetMapping
+    public Iterable<Department> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping(path = "/{title}")
+    public Department find(@PathVariable("title") String username) {
+        return repository.findOne(title);
+    }
+
+    @PostMapping(consumes = "application/json")
+    public Department create(@RequestBody Department department) {
+        return repository.save(department);
+    }
+
+    @DeleteMapping(path = "/{title}")
+    public void delete(@PathVariable("title") String title) {
+        repository.delete(title);
+    }
+
+    @PutMapping(path = "/{username}")
+    public Department update(@PathVariable("username") String username, @RequestBody Department department) throws BadHttpRequest {
+        if (repository.exists(title)) {
+            department.setTitle(title);
+            return repository.save(department);
+        } else {
+            throw new BadHttpRequest();
+        }
+    }
 }
