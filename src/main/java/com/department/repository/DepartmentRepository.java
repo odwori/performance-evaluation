616fb6c45e0046f0218e65ef4aa0fa2b02a45b58
Elijah Ayeeta <elijate@MACBOOKs-MacBook-Pro.local>
2019-03-12 16:23:01 +0300

    Created more methods to Save, edit

--- src/main/java/com/department/repository/DepartmentRepository.java
+++ src/main/java/com/department/repository/DepartmentRepository.java
@@ -3,8 +3,20 @@ package com.department.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import com.department.entity.Department;
+
 
 @RepositoryRestResource(path = "/departments")
 public interface DepartmentRepository extends JpaRepository<Department, String> {
+
+	Department save(Department department);
+
+	void delete(String title);
+
+	boolean exists(String title);
+
+	Department findOne(String title);
+
+	Iterable<Department> findAll();
 
 }
